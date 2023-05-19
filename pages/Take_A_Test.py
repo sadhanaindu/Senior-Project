a6bf0e30974d634517fd6d06f3b60d07085e6c90
Sadhana Indukuri <sadhanaindukuri@Sadhanas-MBP.MG8702>
2023-05-19 15:50:03 -0700

    take test UI

--- pages/Take_A_Test.py
+++ pages/Take_A_Test.py
@@ -14,7 +14,141 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import model
 from model import FacialExpressionModel
 
+
 st.set_page_config(page_title="Take_A_Test")
+
+# Embed CSS styles
+def set_css():
+    st.markdown(
+        """
+        <style>
+        body {
+            background-color: #f1f1f1;
+        }
+
+        h1, h2, h3 {
+            font-family: "Arial", sans-serif;
+            color: #333333;
+        }
+
+        p {
+            font-family: "Verdana", sans-serif;
+            font-size: 14px;
+            color: #555555;
+        }
+
+        .button {
+            background-color: #007bff;
+            color: #ffffff;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .button:hover {
+            background-color: #0056b3;
+        }
+
+        .main-container {
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-start;
+            margin-bottom: 10px;
+        }
+
+        .container {
+            background-color: #ffffff;
+            border: 1px solid #dddddd;
+            padding: 10px;
+            border-radius: 5px;
+        }
+
+        .placeholder-container {
+            flex: 1;
+            margin-right: 10px;
+        }
+
+        .timer-container {
+            flex: 1;
+            margin-right: 10px;
+        }
+
+        .exercise-container {
+            flex: 1;
+        }
+
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+set_css()
+warning_style = """
+    <style>
+    .custom-warning {
+        background-color: #ffcc00;
+        color: #333333;
+        padding: 8px;
+        border-radius: 4px;
+        font-weight: bold;
+    }
+    </style>
+"""
+timer_style = """
+<style>
+.timer-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 100px;
+  margin-bottom: 10px;
+  width: 500px;
+  height: 50px;
+  background-color: #f2f2f2;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+}
+
+.timer-text {
+  font-size: 24px;
+  font-weight: bold;
+  color: #333333;
+}
+</style>
+"""
+
+video_style =  """
+<style>
+.video-container {
+  max-width: 800px;
+  margin: 20px auto;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+}
+
+.video {
+  width: 100%;
+  height: 0;
+  padding-bottom: 56.25%;
+  position: relative;
+}
+
+.video iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
+"""
+# Render the CSS style
+st.markdown(video_style, unsafe_allow_html=True)
+
+st.markdown(timer_style, unsafe_allow_html=True)
+
+st.markdown(warning_style, unsafe_allow_html=True)
+
+
 st.markdown("# Take an Exam")
 st.sidebar.header("Take Exam")
 
@@ -51,19 +185,21 @@ if(submit):
             placeholder.container().warning(pred)
             if pred in {"Happy", "Neutral", "Surprise", "Disgust"}:
                 time.sleep(1)
-                excersise.container().warning("Keep taking the exam!")
                 
 
             else:
-                excersise.container().warning("Do the breathing exercise found below!")
+                excersise.container().markdown('<div class="custom-warning">Do the breathing exercise found above!</div>', unsafe_allow_html=True)
                 vid = random.randint(1, 3)
+                placeholder.write(video_style, unsafe_allow_html=True)
                 if (vid == 1):
                     placeholder.video("https://www.youtube.com/watch?v=5DqTuWve9t8&ab_channel=Calm")
+
                     time.sleep(35)
                     placeholder.empty()
                 
                 if (vid == 2):
                     placeholder.video("https://www.youtube.com/watch?v=xbF4NBTukAI")
+
                     time.sleep(90)
                     placeholder.empty()
                 
@@ -76,7 +212,7 @@ if(submit):
             
             times = times - 1
             
-            timer.container().warning(times)
+            timer.container().markdown(f'<div class="timer-container"><span class="timer-text">{times}</span></div>', unsafe_allow_html=True)
 
 else:
     st.write('Stopped')
